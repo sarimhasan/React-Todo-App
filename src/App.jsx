96f3547b7ae273d6fba107693d97e081087d3108
Sarim Hasam <sarimhasanplace123@gmail.com>
2024-02-26 19:28:35 +0500

    added frontend and the basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,91 @@
+import { useState, useEffect } from "react";
+
+import { FaRegCheckCircle } from "react-icons/fa";
+import { FaRegTrashAlt } from "react-icons/fa";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  function handleTextField(e) {
+    setNewTodo(e.target.value);
+  }
+
+  function handleAdd() {
+    if (newTodo.trim() !== "") {
+      const newTodoItem = { id: todos.length, newTodo, isCompleted: false };
+      setTodos([...todos, newTodoItem]);
+      setNewTodo("");
+    }
+  }
+
+  function handleDone(index) {
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  }
+
+  function handleDelete(index) {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  }
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="main bg-gradient-to-br from-dark-blue from-10% via-light-blue via-30% to-light-greenish-yellow to-90% min-h-screen">
+        <div className="w-2/4 mx-auto py-28">
+          <h1 className="text-3xl text-center font-medium">
+            One Task at a Time.
+          </h1>
+          <div className="inputs mt-6 flex justify-center">
+            <input
+              onChange={handleTextField}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleAdd();
+                }
+              }}
+              value={newTodo}
+              type="text"
+              placeholder="Add a Task..."
+              className="w-[429px] h-[38px] bg-dark-blue border rounded-l-xl p-2 border-none text-slate-300"
+            />
+            <button
+              onClick={handleAdd}
+              className="w-[95px] h-[38px] border border-none rounded-r-xl p-2 bg-slate-300"
+            >
+              I Got this!
+            </button>
+          </div>
+
+          <p className="text-center m-4">2/21/2024, 7:50:50 PM</p>
+
+          {todos.map((todo, index) => {
+            return (
+              <div
+                key={todo.id}
+                className="tasks flex justify-between max-w-max mx-auto py-2 px-3 gap-5 border-2 border-solid rounded-full border-black bg-sea-green mt-4"
+              >
+                <p
+                  className={
+                    todo.isCompleted ? "text-lg line-through" : "text-lg"
+                  }
+                >
+                  {todo.newTodo}
+                </p>
+                <div className="buttons flex gap-2">
+                  <button onClick={() => handleDone(index)}>
+                    <FaRegCheckCircle size={20} />
+                  </button>
+                  <button onClick={() => handleDelete(index)}>
+                    <FaRegTrashAlt size={20} />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-blue": "#265073",
+        "light-blue": "#2D9596",
+        "sea-green": "#9AD0C2",
+        "light-greenish-yellow": "#F1FADA",
+      },
+    },
   },
   plugins: [],
 };
